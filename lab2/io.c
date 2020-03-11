d04d27dd3377852a0ee44622d9d46215d25baef7
TemporaryResident <Patrikos112233@interia.pl>
2020-03-11 14:03:06 +0100

    lab2-start

---
+++ lab2/io.c
@@ -0,0 +1,5 @@
+#include "string_lib.h"
+
+int main() {
+    printf("Hellosc\n");
+}
---
+++ lab2/string_lib.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+char* concat(const char* s1, const char* s2) {
+    int   len    = strlen(s1) + strlen(s2) + 1;
+    char* result = (char*) malloc(len);
+
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
+
+char* concatWithSeparator(const char* s1, const char* s2, const char* separator) {
+    char* temp   = concat(s1, separator);
+    char* result = concat(temp, s2);
+    
+    free(temp);
+    return result;
+}
+
+char* numberToString(int i) {
+    int size = 1;
+    int tmp  = i;
+    while(tmp / 10 > 0) {
+        tmp /= 10;
+        size++;
+    }
+
+    char* buffer = (char*) malloc((size + 1) * sizeof(char));
+
+    for(int j = size - 1; j >= 0; j--) {
+        char psi  = ((i % 10) + '0');
+        buffer[j] = psi;
+        i /= 10;
+    }
+    buffer[size] = '\0';
+
+    return buffer;
+}
+
+char* copyString(const char* str) {
+    int   len    = strlen(str);
+    char* result = (char*) malloc(len);
+
+    strcpy(result, str);
+
+    return result;
+}
+
+int startsWithDigit(const char* str) {
+    return isdigit(str[0]);
+}
+
+int isEmpty(const char* str) {
+    return str[0] == '\0';
+}
+
+int startsWith(const char* str, const char* prefix) {
+    int i = 0;
+    while (str[i] != '\0' && prefix[i] != '\0') {
+        if (str[i] != prefix[i]) {
+            return 0;
+        }
+        i++;
+    }
+
+    if(prefix[i] == '\0') {
+        return 1; 
+    } else {
+        return 0;
+    }
+}
+
+char* randomString(int length) {
+    char* str = calloc(length + 1, sizeof(char));
+
+    for (int i = 0; i < length; i++) {
+        char randomletter = 'a' + (rand() % 26);
+        str[i] = randomletter;
+    }
+
+    str[length] = '\0';
+}
