88f82367e10c9e2b1b3a1ff9692c3bc08cd57c0c
TemporaryResident <Patrikos112233@interia.pl>
2020-03-13 00:14:57 +0100

    finished sort -sys

--- lab2/io.c
+++ lab2/io.c
@@ -44,6 +44,8 @@ void copySys(char* src, char* dest, int recordsNum, int recordSize) {
         write(destFd, buffer, bytes);
     }
 
+    free(buffer);
+
     close(srcFd);
     close(destFd);
 }
@@ -65,9 +67,83 @@ void copyLib(char* src, char* dest, int recordsNum, int recordSize) {
         fwrite(buffer, sizeof(char), bytes, destFilePtr);
     }
 
+    free(buffer);
+
     fclose(srcFilePtr);
     fclose(destFilePtr);
 }
+
+void swapLines(int fd, int i, int j, size_t bytes) {
+    char* bufferI = malloc(sizeof(char) * (bytes + 1));
+    char* bufferJ = malloc(sizeof(char) * (bytes + 1));
+
+    off_t iOff = i * bytes;
+    off_t jOff = j * bytes;
+
+    lseek(fd, iOff, SEEK_SET);
+    read(fd, bufferI, bytes);
+    lseek(fd, jOff, SEEK_SET);
+    read(fd, bufferJ, bytes);
+
+    lseek(fd, iOff, SEEK_SET);
+    write(fd, bufferJ, bytes);
+    lseek(fd, jOff, SEEK_SET);
+    write(fd, bufferI, bytes);
+
+    free(bufferI);
+    free(bufferJ);
+}
+
+int qSortSysPartition(int fd, int from, int to, size_t bytes) {
+    char* pivot = malloc(sizeof(char) * (bytes + 1));
+    char* e     = malloc(sizeof(char) * (bytes + 1));
+
+    lseek(fd, to * bytes, SEEK_SET);
+    read(fd, pivot, bytes);
+
+    int smaller = from - 1;
+    int i       = from - 1;
+    while (++i < to) {
+        lseek(fd, i * bytes, SEEK_SET);
+        read(fd, e, bytes);
+
+        if (strcmp(e, pivot) < 0) {
+            swapLines(fd, ++smaller, i, bytes);
+        }
+    }
+
+    swapLines(fd, ++smaller, i, bytes);
+
+    return smaller;
+}
+
+
+
+void qSortSysRecursive(int fd, int from, int to, size_t bytes) {
+    if (from >= to) {
+        return;
+    }
+
+    int mid = qSortSysPartition(fd, from, to, bytes);
+
+    qSortSysRecursive(fd, from, mid - 1, bytes);
+    qSortSysRecursive(fd, mid + 1, to, bytes);
+}
+
+
+void qSortSys(char* filename, int recordsNum, int recordSize) {
+    int fd = open(filename, O_RDWR);
+
+    size_t bytes = recordSize + 1;
+    int from = 0;
+    int to   = recordsNum - 1;
+
+    qSortSysRecursive(fd, from, to, bytes);
+
+    close(fd);
+}
+
+
 
 int main(int argc, char* argv[]) {
     char msg[250] = "";
@@ -102,6 +178,21 @@ int main(int argc, char* argv[]) {
 
             sprintf(msg, "Command: copy src: \"%s\" to \"%s\", %s records, %s bytes each. Flag: %s \n", argv[i - 4], argv[i - 3], argv[i - 2], argv[i - 1], argv[i]);
             printf("%s", msg);
+        } else if (strcmp("sort", command) == 0) {
+            char* filename   = argv[++i];
+            int   recordsNum = atoi(argv[++i]);
+            int   recordSize = atoi(argv[++i]);
+
+            i += 1;
+
+            if (strcmp("-sys", argv[i]) == 0) {
+                qSortSys(filename, recordsNum, recordSize);
+            } else if (strcmp("-lib", argv[i]) == 0) {
+                error("Not impl");
+            } else {
+                error("Not implemented");
+            }
+
         } else {
             error("Failed to parse command");
         }
