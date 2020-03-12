3242f45f379935aee5bd9c6dfaba712047a8cef8
TemporaryResident <Patrikos112233@interia.pl>
2020-03-12 21:14:37 +0100

    added copySys

--- lab2/io.c
+++ lab2/io.c
@@ -7,8 +7,13 @@
 #include <unistd.h>
 #include <string.h>
 
+void error(char* msg) {
+    printf("Error: %s\n", msg);
+    exit(0);
+}
+
 void generate(char* filename, int recordsNum, int recordSize) {
-    int fd = open(filename, O_CREAT | O_WRONLY, S_IRWXU);
+    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 
     while (recordsNum-- > 0) {
         char* rand = randomString(recordSize);
@@ -23,24 +28,58 @@ void generate(char* filename, int recordsNum, int recordSize) {
     close(fd);
 }
 
+void copySys(char* src, char* dest, int recordsNum, int recordSize) {
+    int srcFd = open(src, O_RDONLY);
+
+    if (srcFd == -1) {
+        error("Source file does not exist");
+    }
+
+    int destFd = open(dest, O_CREAT | O_WRONLY | O_APPEND, S_IRWXU);
+    char* buffer = (char*) malloc(sizeof(char) * (recordSize + 2));
+    size_t bytes = recordSize + 1;
+
+    while (recordsNum-- > 0) {
+        read(srcFd, buffer, bytes);
+        write(destFd, buffer, bytes);
+    }
+
+    close(srcFd);
+    close(destFd);
+}
+
 int main(int argc, char* argv[]) {
     char msg[250] = "";
-    int i = 1;
-    while (i < argc) {
+    int i = 0;
+    while (++i < argc) {
         const char* command = argv[i];
+        printf("%s\n", command);
         
         if (strcmp("generate", command) == 0) {
             char* filename   = argv[++i];
             int   recordsNum = atoi(argv[++i]);
             int   recordSize = atoi(argv[++i]);
 
-            sprintf(msg, "Command: generate %s %s %s\n", argv[i - 2], argv[i - 1], argv[i]);
-            generate(filename, recordsNum, recordSize);
+            sprintf(msg, "Command: generate file \"%s\" with %s records, %s bytes each. \n", argv[i - 2], argv[i - 1], argv[i]);
             printf("%s", msg);
-            printf("Generated %s\n", filename);
+            generate(filename, recordsNum, recordSize);
+        } else if (strcmp("copy", command) == 0) { 
+            char* src   = argv[++i];
+            char* dest  = argv[++i];
+            int   recordsNum = atoi(argv[++i]);
+            int   recordSize = atoi(argv[++i]);
+
+            if (strcmp("-sys", argv[++i]) == 0) {
+                copySys(src, dest, recordsNum, recordSize);
+            } else {
+                error("Not implemented");
+            }
+
         } else {
-            exit(0);
+            error("Failed to parse command");
         }
+
+        printf("Done\n");
     }
 
     return 0;
